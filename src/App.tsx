2a1a02ed849bf3ccaf1817c5ca0c53dd48786a83
junk237 <reds7.buddy42rich@gmail.com>
2023-09-11 21:21:19 +0900

    feat: Added the ability to display restaurant information obtained from the backend API

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,41 @@
 import React, { useEffect, useState } from "react"
 
-import { execTest } from "lib/api/test"
+import { showRestaurants } from "lib/api/restaurants"
 
 const App: React.FC = () => {
-  const [message, setMessage] = useState<string>("")
+  const [restaurants, setRestaurants] = useState<any[]>([]);
 
-  const handleExecTest = async () => {
-    const res = await execTest()
-
-    if (res.status === 200) {
-      setMessage(res.data.message)
+  const handleShowRestaurants = async () => {
+    try {
+      const res = await showRestaurants();
+      if (res && res.status === 200) {
+        // レストランのデータを取得
+        const restaurantData = res.data.message;
+        setRestaurants(restaurantData);
+      }
+    } catch (error) {
+      console.error("レストランデータの取得に失敗しました:", error);
     }
-  }
+  };
 
   useEffect(() => {
-    handleExecTest()
-  }, [])
+    handleShowRestaurants();
+  }, []);
 
   return (
-    <h1>{message}</h1>
-  )
-}
+    <div>
+      <h1>レストラン一覧</h1>
+      <ul>
+        {restaurants.map((restaurant: any, index) => (
+          <li key={index}>
+            <h2>{restaurant.name}</h2>
+            <p>住所: {restaurant.vicinity}</p>
+            {/* 他のレストラン情報も表示できるように修正 */}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
 
-export default App
+export default App;
