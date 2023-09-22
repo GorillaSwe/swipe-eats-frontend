08df5bdb780ed6a25ec0bcdae6705e7e91caccaa
junk237 <reds7.buddy42rich@gmail.com>
2023-09-22 21:02:42 +0900

    [add]スワイプによりレストランを切り替える処理を実装

--- src/utils/api/restaurants.ts
+++ src/utils/api/restaurants.ts
@@ -1,15 +1,18 @@
 import client from "utils/api/client"
 
 // 動作確認用
-export const showRestaurants = async () => {
+export const showRestaurants = async (latitude: number, longitude: number) => {
   try {
-    const response = await client.get("/restaurants");
+    const response = await client.get("/restaurants", {
+      params: { latitude, longitude },
+    });
     if (response.status === 200) {
-      return response; // ここではデータをそのまま返す
+      return response.data;
     } else {
       throw new Error("APIからデータを取得できませんでした。");
     }
   } catch (error) {
     console.error(error);
+    throw error;
   }
 };
