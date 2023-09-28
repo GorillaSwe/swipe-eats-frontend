ac9ae510dae88dafe1b8c56c40c649c1ef6f69dc
junk237 <reds7.buddy42rich@gmail.com>
2023-09-29 04:32:48 +0900

    [add]検索結果ページからマップ画面に遷移するよう修正
    検索結果ページからマップ画面に遷移し、マーカーを配置するよう修正

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Routes, Route } from "react-router-dom";
 import SearchPage from "./components/SearchPage";
 import ResultsPage from "./components/ResultsPage";
 import CategorySelectionPage from "./components/CategorySelectionPage";
+import MapPage from "./components/MapPage";
 import './App.css'
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
       <Route path="/" element={<CategorySelectionPage />} />
       <Route path="/search" element={ <SearchPage /> } />
       <Route path="/results" element={ <ResultsPage /> } />
+      <Route path="/map" element={ <MapPage /> } />
     </Routes>
   );
 }
